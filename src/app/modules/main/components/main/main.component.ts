89bfb86bee5a3ab06487fca027c6cce3dcc89b76
Emilio <emiliosebastianp@gmail.com>
2019-01-21 21:37:40 -0300

    main component se agrega disparador sidebar menu

--- src/app/modules/main/components/main/main.component.ts
+++ src/app/modules/main/components/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $;
 
 @Component({
   selector: 'app-main',
@@ -10,6 +11,9 @@ export class MainComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    $('.sidebar-menu').tree();
+    
   }
 
 }
